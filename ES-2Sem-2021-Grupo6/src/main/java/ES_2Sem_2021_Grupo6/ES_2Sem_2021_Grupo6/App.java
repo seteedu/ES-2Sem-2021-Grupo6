4810bbfd6eefdbde3875f901e15088155d7300c5
seteedu <seteedu@gmail.com>
2021-03-16 16:50:36 +0000

    primeiro

---
+++ ES-2Sem-2021-Grupo6/src/main/java/ES_2Sem_2021_Grupo6/ES_2Sem_2021_Grupo6/App.java
@@ -0,0 +1,13 @@
+package ES_2Sem_2021_Grupo6.ES_2Sem_2021_Grupo6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
