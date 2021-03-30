6df59f3f87a96df53a664d8cd191add900ba87f6
mmelo123 <mjrmo2@iscte-iul.pt>
2021-03-30 15:46:12 +0100

    GUI update

--- ES-2Sem-2021-Grupo6/src/main/java/GUI/Menu.java
+++ ES-2Sem-2021-Grupo6/src/main/java/GUI/Menu.java
@@ -21,42 +21,34 @@ import java.io.File;
 import javax.swing.JTextField;
 import java.awt.Toolkit;
 import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JInternalFrame;
+import javax.swing.JSplitPane;
 
 public class Menu extends JFrame {
 	private JTextField textField;
 	public Menu() {
+		setResizable(false);
 		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Utilizador\\git\\ES-2Sem-2021-Grupo6\\ES-2Sem-2021-Grupo6\\iscte_logo.png"));
 		getContentPane().setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		setSize(1000,800);
+		setSize(900,500);
 		setFont(new Font("Times New Roman", Font.PLAIN, 12));
 		setForeground(Color.LIGHT_GRAY);
 		setTitle("Projeto ES");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] {30, 30, 30, 30, 30, 0, 0, 30};
-		gridBagLayout.rowHeights = new int[] {30, 30, 30, 30, 30, 30, 0, 0, 0, 30};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		getContentPane().setLayout(gridBagLayout);
+		getContentPane().setLayout(null);
 
 		JLabel lblNewLabel = new JLabel("Projeto ES");
+		lblNewLabel.setBounds(561, 156, 199, 69);
 		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Utilizador\\git\\ES-2Sem-2021-Grupo6\\ES-2Sem-2021-Grupo6\\iscte_logo.png"));
-		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 26));
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 26));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.gridheight = 3;
-		gbc_lblNewLabel.gridwidth = 7;
-		gbc_lblNewLabel.fill = GridBagConstraints.BOTH;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
-		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 0;
-		getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+		getContentPane().add(lblNewLabel);
 
 		JButton btnNewButton = new JButton("Selecione um projeto Java");
-		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 12));
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void mouseClicked(MouseEvent arg0) {
 				JFileChooser fileChooser = new JFileChooser();
 				int returnValue = fileChooser.showOpenDialog(null);
 				if(returnValue == JFileChooser.APPROVE_OPTION) {
@@ -65,30 +57,33 @@ public class Menu extends JFrame {
 				}
 			}
 		});
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.fill = GridBagConstraints.BOTH;
-		gbc_btnNewButton.gridheight = 2;
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.gridwidth = 5;
-		gbc_btnNewButton.gridx = 1;
-		gbc_btnNewButton.gridy = 3;
-		getContentPane().add(btnNewButton, gbc_btnNewButton);
+		btnNewButton.setBounds(487, 256, 168, 43);
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		
+		getContentPane().add(btnNewButton);
 		
 		textField = new JTextField();
-		textField.setToolTipText("DAVID DEGUSTADOR DE PIROCA");
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.gridheight = 2;
-		gbc_textField.insets = new Insets(0, 0, 5, 0);
-		gbc_textField.fill = GridBagConstraints.BOTH;
-		gbc_textField.gridx = 6;
-		gbc_textField.gridy = 3;
-		getContentPane().add(textField, gbc_textField);
+		textField.setBounds(665, 262, 199, 30);
+		textField.setToolTipText("");
+		getContentPane().add(textField);
 		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Utilizador\\Desktop\\istaLogo.png"));
+		lblNewLabel_1.setBounds(0, 0, 450, 471);
+		getContentPane().add(lblNewLabel_1);
+		
+		JButton btnNewButton_1 = new JButton("Extrair métricas");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_1.setBounds(710, 319, 107, 23);
+		getContentPane().add(btnNewButton_1);
 		
 	}
 
 	public static void main(String[] args) {
 		new Menu().setVisible(true);
 	}
-	
 }
