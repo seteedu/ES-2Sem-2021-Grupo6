bdb4db5052d6b279ec0624e93f613e01b339af0d
daavidsantos <daavidc.santos@gmail.com>
2021-04-02 17:15:30 +0100

    Class Result -> Excel

---
+++ ES-2Sem-2021-Grupo6/src/main/java/ES_2Sem_2021_Grupo6/ES_2Sem_2021_Grupo6/Result.java
@@ -0,0 +1,38 @@
+package ES_2Sem_2021_Grupo6.ES_2Sem_2021_Grupo6;
+
+public class Result {
+	private String package1;
+	private String class1;
+	private String method1;
+	private int nom;
+	private int loc;
+	private int wmc;
+	private int locm;
+	private int cyclom;
+		
+	public String getPackage1(){
+		return package1;
+	}
+	public String getClass1(){
+		return class1;
+	}
+	public String getMethod1(){
+		return method1;
+	}
+	public int getNom() {
+		return nom;
+	}
+	public int getLoc() {
+		return loc;
+	}
+	public int getWmc() {
+		return wmc;
+	}
+	public int getLocm() {
+		return locm;
+	}
+	public int getCyclom() {
+		return cyclom;
+	}
+
+}
