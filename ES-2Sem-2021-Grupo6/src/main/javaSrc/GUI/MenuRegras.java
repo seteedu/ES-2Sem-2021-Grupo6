8c30427902232ce33f26f7204b380ea2db59f419
MachadoS21 <sbcmo@iscte-iul.pt>
2021-05-04 16:46:19 +0100

    Excessões da GUI (modificar regras)

--- ES-2Sem-2021-Grupo6/src/main/javaSrc/GUI/MenuRegras.java
+++ ES-2Sem-2021-Grupo6/src/main/javaSrc/GUI/MenuRegras.java
@@ -23,6 +23,7 @@ import java.awt.event.WindowListener;
 import java.util.ArrayList;
 
 import java.awt.Font;
+import java.awt.TextField;
 import java.awt.Color;
 import java.awt.Component;
 import javax.swing.SwingConstants;
@@ -35,35 +36,35 @@ public class MenuRegras extends JFrame {
 	private JComboBox<Object> comboBox_Metrica;
 	private JComboBox<?> comboBox_logica;
 	private JComboBox<?> comboBox_Sinal;
-	private String[] codeSmell = {"is_God_Class","is_Long_Method"};
-	private String[] metricasClasse = {"LOC_Class","WMC_Class","NOM_Class"};
-	private String[] metricasMetodos = {"LOC_Method","CYCLO_Method"};
+	private String[] codeSmell = { "is_God_Class", "is_Long_Method" };
+	private String[] metricasClasse = { "LOC_Class", "WMC_Class", "NOM_Class" };
+	private String[] metricasMetodos = { "LOC_Method", "CYCLO_Method" };
 	private JComboBox<?> comboBox = new JComboBox<Object>(codeSmell);
 	private ArrayList<Threshold> arraylist = new ArrayList<>();
 
-	private Object makeObj(final String item)  {
-		return new Object() { public String toString() { return item; } };
+	private Object makeObj(final String item) {
+		return new Object() {
+			public String toString() {
+				return item;
+			}
+		};
 	}
-
 
 	public MenuRegras(MainMenu mainmenu, RuleSet rs) {
 		setResizable(false);
-		setSize(900,500);
+		setSize(900, 500);
 		getContentPane().setLayout(null);
-
 
 		setLocationRelativeTo(null);
 		WindowListener exitListener = new WindowAdapter() {
 
-		    @Override
-		    public void windowClosing(WindowEvent e) {
-		    	rs.writeFile(MainMenu.FILE_PATH);
-		    	System.exit(0);
-		    }
+			@Override
+			public void windowClosing(WindowEvent e) {
+				rs.writeFile(MainMenu.FILE_PATH);
+				System.exit(0);
+			}
 		};
 		this.addWindowListener(exitListener);
-
-
 
 		JPanel panel_1 = new JPanel();
 		getContentPane().add(panel_1);
@@ -108,34 +109,39 @@ public class MenuRegras extends JFrame {
 		btnNewButton_1.setBounds(617, 233, 46, 23);
 		panel_1.add(btnNewButton_1);
 
-		ActionListener confirmActionListener = new ActionListener() {//add actionlistner to listen for change
+		ActionListener confirmActionListener = new ActionListener() {// add actionlistner to listen for change
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String[] s = textField_2.getText().split(" ");
-				if(s[s.length-1].equals("E") || (s[s.length-1].equals("OU") ) || s[s.length-1].equals("")) {
-					//DAR ARRAY LIST DE THRESHOLDS
-					lbl_sucesso.setVisible(false);
-					lblARegraNo.setText("A regra não pode terminar numa proposição logica");
+
+				if (rs.getHashMap().containsKey(textField.getText())) {
+					lblARegraNo.setText("A regra com esse nome já existe!");
 					lblARegraNo.setVisible(true);
 				} else {
-					if(textField.getText().equals("")) {
-						lblARegraNo.setText("Por favor dê um título à regra!");
+					String[] s = textField_2.getText().split(" ");
+					if (s[s.length - 1].equals("E") || (s[s.length - 1].equals("OU")) || s[s.length - 1].equals("")) {
+						// DAR ARRAY LIST DE THRESHOLDS
+						lbl_sucesso.setVisible(false);
+						lblARegraNo.setText("A regra não pode terminar numa proposição logica");
 						lblARegraNo.setVisible(true);
-					}else {
-						lblARegraNo.setVisible(false);
-						lbl_sucesso.setVisible(true);
-						//RETORNAR ARRAY LIST PARA BACKEND
-						Rule rule = new Rule (textField.getText(), comboBox.getSelectedItem().toString(), arraylist);
-						rs.addRule(rule);
-						arraylist = new ArrayList<>();
-						textField.setText("");
-						textField_2.setText("");
-						comboBox.setEnabled(true);
-						btnNewButton_1.setEnabled(true);
+					} else {
+						if (textField.getText().equals("")) {
+							lblARegraNo.setText("Por favor dê um título à regra!");
+							lblARegraNo.setVisible(true);
+						} else {
+							lblARegraNo.setVisible(false);
+							lbl_sucesso.setVisible(true);
+							// RETORNAR ARRAY LIST PARA BACKEND
+							Rule rule = new Rule(textField.getText(), comboBox.getSelectedItem().toString(), arraylist);
+							rs.addRule(rule);
+							arraylist = new ArrayList<>();
+							textField.setText("");
+							textField_2.setText("");
+							comboBox.setEnabled(true);
+							btnNewButton_1.setEnabled(true);
+						}
 					}
+
 				}
-
-
 			}
 		};
 
@@ -159,27 +165,24 @@ public class MenuRegras extends JFrame {
 		comboBox.setBounds(595, 62, 117, 20);
 		panel_1.add(comboBox);
 
-
 		JPanel panel = new JPanel();
 		panel.setBounds(156, 129, 610, 94);
 		panel_1.add(panel);
 		panel.setLayout(null);
 
 		addComponents(panel);
-		
+
 		JLabel lblNewLabel_5 = new JLabel("E/OU:");
 		lblNewLabel_5.setBounds(500, 27, 45, 13);
 		panel.add(lblNewLabel_5);
 
-
-
-		ActionListener cbActionListener = new ActionListener() {//add actionlistner to listen for change
+		ActionListener cbActionListener = new ActionListener() {// add actionlistner to listen for change
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				String s = (String) comboBox.getSelectedItem();//get the selected item
+				String s = (String) comboBox.getSelectedItem();// get the selected item
 
-				switch (s) {//check for a match
+				switch (s) {// check for a match
 				case "is_God_Class":
 					comboBox_Metrica.removeAllItems();
 					comboBox_Metrica.addItem(makeObj("LOC_Class"));
@@ -198,7 +201,6 @@ public class MenuRegras extends JFrame {
 
 		comboBox.addActionListener(cbActionListener);
 
-
 		JLabel ifLabel = new JLabel("SE (");
 		ifLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		ifLabel.setBounds(144, 267, 45, 43);
@@ -208,7 +210,7 @@ public class MenuRegras extends JFrame {
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lblNewLabel_4.setBounds(634, 273, 45, 31);
 		panel_1.add(lblNewLabel_4);
-		
+
 		JButton btnLimpar = new JButton("Limpar Regra");
 		btnLimpar.addMouseListener(new MouseAdapter() {
 			@Override
@@ -222,24 +224,29 @@ public class MenuRegras extends JFrame {
 				btnNewButton_1.setEnabled(true);
 			}
 		});
-		
+
 		btnLimpar.setBounds(378, 363, 117, 21);
 		panel_1.add(btnLimpar);
 
 		btnNewButton_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				comboBox.setEnabled(false);;
+				comboBox.setEnabled(false);
+				;
 				try {
-					if(!textField_2.getText().equals("")) {
+					if (!textField_2.getText().equals("")) {
 						String[] s = textField_2.getText().split(" ");
-						if( ! ((s[s.length-1].equals("E")) || (s[s.length-1].equals("OU") )) ) {
+						if (!((s[s.length - 1].equals("E")) || (s[s.length - 1].equals("OU")))) {
 							btnNewButton_1.setEnabled(false);
-							lblARegraNo.setText("Não pode adicionar mais proposições! Por favor confirme ou limpe a regra.");
+							lblARegraNo.setText(
+									"Não pode adicionar mais proposições! Por favor confirme ou limpe a regra.");
 							lblARegraNo.setVisible(true);
 						} else {
 							lblARegraNo.setVisible(false);
-							createThreshold(comboBox_Metrica.getSelectedItem().toString(), comboBox_Sinal.getSelectedItem().toString(), Integer.parseInt(textField_1.getText() ), comboBox_logica.getSelectedItem().toString());
+							createThreshold(comboBox_Metrica.getSelectedItem().toString(),
+									comboBox_Sinal.getSelectedItem().toString(),
+									Integer.parseInt(textField_1.getText()),
+									comboBox_logica.getSelectedItem().toString());
 							writeToString();
 							panel.removeAll();
 							addComponents(panel);
@@ -248,7 +255,9 @@ public class MenuRegras extends JFrame {
 						}
 					} else {
 						lblARegraNo.setVisible(false);
-						createThreshold(comboBox_Metrica.getSelectedItem().toString(), comboBox_Sinal.getSelectedItem().toString(), Integer.parseInt(textField_1.getText() ), comboBox_logica.getSelectedItem().toString());
+						createThreshold(comboBox_Metrica.getSelectedItem().toString(),
+								comboBox_Sinal.getSelectedItem().toString(), Integer.parseInt(textField_1.getText()),
+								comboBox_logica.getSelectedItem().toString());
 						writeToString();
 						panel.removeAll();
 						addComponents(panel);
@@ -256,7 +265,7 @@ public class MenuRegras extends JFrame {
 						panel.repaint();
 					}
 				} catch (Exception e) {
-					//FAZER CATCH DO ERRO
+					// FAZER CATCH DO ERRO
 					lblARegraNo.setText("O limites têm de ser numeros!");
 					lblARegraNo.setVisible(true);
 				}
@@ -265,21 +274,21 @@ public class MenuRegras extends JFrame {
 		});
 	}
 
-	//-------------------------------------------------------------
+	// -------------------------------------------------------------
 
-	private void addComponents(JPanel panel){
+	private void addComponents(JPanel panel) {
 
 		JLabel lblNewLabel_2 = new JLabel("Métrica:");
 		lblNewLabel_2.setBounds(31, 26, 46, 14);
 		panel.add(lblNewLabel_2);
 
-		if(comboBox.getSelectedItem().equals("isLongMethod")) {
+		if (comboBox.getSelectedItem().equals("isLongMethod")) {
 			comboBox_Metrica = new JComboBox<Object>(metricasMetodos);
-		} else if (comboBox.getSelectedItem().equals("is_God_Class") ){
+		} else if (comboBox.getSelectedItem().equals("is_God_Class")) {
 			comboBox_Metrica = new JComboBox<Object>(metricasClasse);
 		}
 
-		String[] ss = {"","E","OU"};
+		String[] ss = { "", "E", "OU" };
 		comboBox_logica = new JComboBox<Object>(ss);
 		comboBox_logica.setBounds(500, 50, 86, 21);
 		panel.add(comboBox_logica);
@@ -287,7 +296,7 @@ public class MenuRegras extends JFrame {
 		comboBox_Metrica.setBounds(10, 50, 180, 20);
 		panel.add(comboBox_Metrica);
 
-		String[] s = {"<",">"};
+		String[] s = { "<", ">" };
 		comboBox_Sinal = new JComboBox<Object>(s);
 		comboBox_Sinal.setBounds(245, 50, 65, 20);
 		panel.add(comboBox_Sinal);
@@ -301,18 +310,19 @@ public class MenuRegras extends JFrame {
 		lblNewLabel_3.setBounds(391, 26, 46, 14);
 		panel.add(lblNewLabel_3);
 
-
 	}
 
 	private void writeToString() {
-		String newS =  textField_2.getText()+ " " + comboBox_Metrica.getSelectedItem()+ " " + comboBox_Sinal.getSelectedItem()+ " " + textField_1.getText() + " " + comboBox_logica.getSelectedItem() ;
+		String newS = textField_2.getText() + " " + comboBox_Metrica.getSelectedItem() + " "
+				+ comboBox_Sinal.getSelectedItem() + " " + textField_1.getText() + " "
+				+ comboBox_logica.getSelectedItem();
 		textField_2.setText(newS);
 
 	}
 
-	private void createThreshold(String name,  String math, int value, String logic) {
-		if(logic.equals("")) {
-			Threshold t = new Threshold(name,math,value);
+	private void createThreshold(String name, String math, int value, String logic) {
+		if (logic.equals("")) {
+			Threshold t = new Threshold(name, math, value);
 			arraylist.add(t);
 		} else {
 			Threshold t = new Threshold(name, math, value, logic);
